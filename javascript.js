1ff26b78eb9220c824bc0c979f49cc2e55f0ec58
Patrick Farley <patrickfarley1994@yahoo.com>
2018-03-16 01:36:57 -0400

    gifs now load when new button added

--- javascript.js
+++ javascript.js
@@ -1,33 +1,103 @@
 // START JS/JQ FOR TOP BUTTONS POPULATION AND RIGHTSIDE FORUM ENTRY
-    // Initial array of animals //
-    var animals = ["Dog", "Cat", "Fish", "Lizard", "Chicken", "Duck", "Bird", "Elephant", "Dolphin", "Lion"];
-
-    // Function for displaying animal data
-    function renderButtons() {
-        // preventing repeat
-        $("#animals-button").empty();
+// Initial array of animals //
 
 
-        for (var i = 0; i < animals.length; i++) {
-            var a = $("<button>");
-            a.addClass("animal");
-            a.attr("data-name", animals[i]);
-            a.text(animals[i]);
-            $("#animals-button").append(a);
+let animals = ["Dog", "Cat", "Fish", "Lizard", "Chicken", "Duck", "Bird", "Elephant", "Dolphin", "Lion"];
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&api_key=m5KCIGAcXgixGVIBlD4YGKWhjiAeCRU8&limit=10";
+
+
+//GET request
+$.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+
+    //data comes back from GET response ".then"
+    .then(function (response) {
+        console.log(queryURL)
+
+        var results = response.data;
+        for (var i = 0; i < 10; i++) {
+
+            var animalsDiv = $("<div>"); //jquery to make div
+            var animalsImg = $("<img>"); //jquery to make place for images
+            animalsImg.attr("src", results[i].images.fixed_height.url);
+
+
+            $("button").click(function () {
+                animalsDiv.append(animalsImg);
+                $("#gif-space").prepend(animalsDiv)
+
+            });
+
+            
+          
+
         }
-    }
 
-    //adding animal once button is clicked
-    $("#add-animal").on("click", function (event) {
-        event.preventDefault();
-        var animal = $("#animal-input").val()
-        animals.push(animal);
-        renderButtons();
     });
 
-    // renders buttons at start. 
+// Function for displaying animal data
+function renderButtons() {
+    // preventing repeat
+    $("#animals-button").empty();
+
+
+    for (var i = 0; i < animals.length; i++) {
+        var a = $("<button>");
+        a.addClass("animal");
+        a.attr("data-name", animals[i]);
+        a.text(animals[i]);
+        $("#animals-button").append(a);
+    }
+}
+
+//adding animal once button is clicked
+$("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    var animal = $("#animal-input").val()
+    animals.push(animal);
     renderButtons();
-// END FOR TOP BUTTONS POPULATION AND RIGHTSIDE FORUM ENTRY  
+
+    //SCRIPT FOR BUTTON CLICK AND GIFY POPULATION 
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=m5KCIGAcXgixGVIBlD4YGKWhjiAeCRU8&limit=10";
+
+    //GET request
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
 
-//SCRIPT FOR BUTTON CLICK AND GIFY POPULATION 
+        //data comes back from GET response ".then"
+        .then(function (response) {
+            console.log(queryURL)
+
+            var results = response.data;
+            for (var i = 0; i < 10; i++) {
+
+                var animalDiv = $("<div>"); //jquery to make div
+                var animalImg = $("<img>"); //jquery to make place for images
+                animalImg.attr("src", results[i].images.fixed_height.url);
+
+
+                $("button").click(function () {
+                    animalDiv.append(animalImg);
+                    $("#gif-space").prepend(animalDiv)
+    
+                });
+
+                
+              
+
+            }
+
+        });
+});
+
+
+// renders buttons at start. 
+renderButtons();
